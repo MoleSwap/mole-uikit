335c3aa12eda34030295838f41b9c1ff73649d29
bossmole <info@moleswap.finance>
2021-03-10 21:49:48 +0100

    Update GitHub, Telegram & Twitter redirects

--- src/widgets/Menu/config.ts
+++ src/widgets/Menu/config.ts
@@ -99,7 +99,7 @@ export const links = [
       // },
       {
         label: "Github",
-        href: "https://github.com/goosedefi/",
+        href: "https://github.com/MoleSwap/",
       },
       {
         label: "Docs",
@@ -120,16 +120,16 @@ export const socials = [
     items: [
       {
         label: "English",
-        href: "https://t.me/goosedefi",
+        href: "https://t.me/MoleSwap",
       },
       // {
       //   label: "Bahasa Indonesia",
       //   href: "https://t.me/PancakeSwapIndonesia",
       // },
-      {
-        label: "中文",
-        href: "https://t.me/goosefinancechinese",
-      },
+      // {
+      //   label: "中文",
+      //   href: "https://t.me/goosefinancechinese",
+      // },
       // {
       //   label: "Tiếng Việt",
       //   href: "https://t.me/PancakeSwapVN",
@@ -138,44 +138,44 @@ export const socials = [
       //   label: "Italiano",
       //   href: "https://t.me/pancakeswap_ita",
       // },
-      {
-        label: "русский",
-        href: "https://t.me/goosefinancerussian",
-      },
-      {
-        label: "Türkiye",
-        href: "https://t.me/GooseFinanceTurkey",
-      },
+      // {
+      //   label: "русский",
+      //   href: "https://t.me/goosefinancerussian",
+      // },
+      // {
+      //   label: "Türkiye",
+      //   href: "https://t.me/GooseFinanceTurkey",
+      // },
       // {
       //   label: "Português",
       //   href: "https://t.me/PancakeSwapPortuguese",
       // },
-      {
-        label: "Español",
-        href: "https://t.me/goosefinancespanish",
-      },
-      {
-        label: "日本語",
-        href: "https://t.me/goosefinancejapanese",
-      },
+      // {
+      //   label: "Español",
+      //   href: "https://t.me/goosefinancespanish",
+      // },
+      // {
+      //   label: "日本語",
+      //   href: "https://t.me/goosefinancejapanese",
+      // },
       // {
       //   label: "Français",
       //   href: "https://t.me/pancakeswapfr",
       // },
       {
         label: "Announcements",
-        href: "https://t.me/goosefinanceann",
+        href: "https://t.me/MoleSwapAnn",
       },
-      {
-        label: "Price Bot",
-        href: "https://t.me/BinanceRocketEGG",
-      },
+      // {
+      //   label: "Price Bot",
+      //   href: "https://t.me/BinanceRocketEGG",
+      // },
     ],
   },
   {
     label: "Twitter",
     icon: "TwitterIcon",
-    href: "https://twitter.com/FinanceGoose",
+    href: "https://twitter.com/MoleSwap",
   },
 ];
 
